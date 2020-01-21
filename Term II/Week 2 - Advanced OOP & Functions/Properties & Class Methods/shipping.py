f5cb60cb84f1e59f04bd11da4d94a6d6765a8ac6
glopez21 <glopez21@live.com>
2020-01-21 15:47:41 -0500

    adding code!

--- Term II/Week 2 - Advanced OOP & Functions/Properties & Class Methods/shipping.py
+++ Term II/Week 2 - Advanced OOP & Functions/Properties & Class Methods/shipping.py
@@ -1,12 +1,26 @@
 class ShippingContainer:
     
     next_serial = 1337
+
+# checkpoint 2
+# work on terminal
+   
+    @staticmethod
+    def _get_next_serial():
+        result = ShippingContainer.next_serial
+        ShippingContainer.next_serial += 1
+        return result
+    
+ # Part 1   
     def __ini__(self, owner_code, contents):
-        self,owner_code = owner_code
+        self.owner_code = owner_code
         self.contents = contents
         self.serial = ShippingContainer.next_serial
-        ShippingContainer.next_serial += 1
-        
-        
+        self.next_serial += 1
+        #ShippingContainer.next_serial += 1
+
+# Checkpoint   1     
+# Work on terminal 
+       
         
         
