2be2238dbc502d86cd0fd46db5746e02b84cefbb
glopez21 <glopez21@live.com>
2020-01-27 12:56:20 -0500

    accountID base code

---
+++ Term II/Week 2 - Advanced OOP & Functions/Bank App Project/accountID.py
@@ -0,0 +1,26 @@
+# Though the name of the script is set as AccountID, this particular application or script
+# will denote account number, first and last name of the account holder.count
+
+class Account:
+    transaction_counter = itertools.count(100)
+    
+    def __init__(self, account_number, first_name, last_name):
+    
+    @property
+    def account_number(self):
+    
+    
+    @property 
+    def first_name(self):
+    
+    @first_name.setter
+    def first_name(self, value):
+        
+    @property
+    def last_name(self):
+    
+    @last_name.setter
+    def last_name(self, value):
+        
+    @property
+    def full_name(self):
