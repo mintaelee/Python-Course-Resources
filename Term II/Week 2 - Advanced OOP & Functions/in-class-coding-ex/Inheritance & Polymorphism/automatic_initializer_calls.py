7b78447573a85ea0129a2cd5a31648904ad5c9b9
glopez21 <glopez21@live.com>
2020-01-27 06:48:56 -0500

    added inheritance code

---
+++ Term II/Week 2 - Advanced OOP & Functions/in-class-coding-ex/Inheritance & Polymorphism/automatic_initializer_calls.py
@@ -0,0 +1,10 @@
+class Base1:
+    def __init__(self):
+        print('Base1.__init__')
+        
+class Base2:
+    def __init__(self):
+        print('Base2.__init__')
+        
+class Sub(Base1, Base2):
+    pass
---
+++ Term II/Week 2 - Advanced OOP & Functions/in-class-coding-ex/Inheritance & Polymorphism/base.py
@@ -0,0 +1,28 @@
+# Example of a base class from The Python Journeyman book
+# Chapter 8 p. 253
+
+class Base:
+    def __init__(self):
+        print('Base initializer')
+        
+    def f(self):
+        print('Base.f()')
+
+
+class Sub(Base):
+    pass
+
+# class Sub(Base):
+#     def f(self):
+#         print('Sub.f()')
+
+
+
+# # Subclass initializers
+
+# class Sub(Base):
+#     def __init__(self):
+#         print('Sub initializer')
+        
+#     def f(self):
+#         print('Sub.f()')
