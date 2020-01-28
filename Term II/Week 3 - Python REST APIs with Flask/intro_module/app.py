1f2c66e7c73fae188357b7224998289d7745868c
glopez21 <glopez21@live.com>
2020-01-28 15:45:01 -0500

    none

--- Term II/Week 3 - Python REST APIs with Flask/intro_module/app.py
+++ Term II/Week 3 - Python REST APIs with Flask/intro_module/app.py
@@ -2,8 +2,7 @@ from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
-stores = 
-[{
+stores = [{
     'name': 'My Store',
     'items': [{'name':'my item', 'price': 15.99 }]
 }]
@@ -12,7 +11,6 @@ stores =
 def home():
   return render_template('index.html')
 
-#post /store data: {name :}
 
 @app.route('/store' , methods=['POST'])
 def create_store():
@@ -20,9 +18,7 @@ def create_store():
   new_store = {'name':request_data['name'], 'items':[]}
   stores.append(new_store)
   return jsonify(new_store)
-  #pass
 
-#get /store/<name> data: {name :}
 
 @app.route('/store/<string:name>')
 def get_store(name):
@@ -30,17 +26,12 @@ def get_store(name):
     if store['name'] == name:
           return jsonify(store)
   return jsonify ({'message': 'store not found'})
-  #pass
 
-
-#get /store
 
 @app.route('/store')
 def get_stores():
   return jsonify({'stores': stores})
-  #pass
 
-#post /store/<name> data: {name :}
 
 @app.route('/store/<string:name>/item' , methods=['POST'])
 def create_item_in_store(name):
@@ -50,12 +41,8 @@ def create_item_in_store(name):
         new_item = {'name': request_data['name'], 'price': request_data['price']}
         store['items'].append(new_item)
         return jsonify(new_item)
-  return jsonify ({'message' :'store not found'})
-  #pass
+  return jsonify ({'message' :'store does not exist'})
 
-
-
-#get /store/<name>/item data: {name :}
 
 @app.route('/store/<string:name>/item')
 def get_item_in_store(name):
